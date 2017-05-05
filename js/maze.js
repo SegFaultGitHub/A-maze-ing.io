b3eb0576ed1cdd8e450aac64366338cb8b817f35
Thomas Venner <thomas.venner@ubisoft.com>
2017-05-05 17:28:30 +0200

    First try

--- js/maze.js
+++ js/maze.js
@@ -7,12 +7,11 @@ function maze(width, height) {
 		for (var i = 0; i < this.width; i++) {
 			this.cellArray[i] = new Array(height);
 			for (var j = 0; j < this.height; j++) {
-				cellArray[i][j] = new cell();
-				console.log(cellArray[i][j]);
+				this.cellArray[i][j] = new cell();
 			}
 		}
 		this.path = [];
-		path.push({
+		this.path.push({
 			x: random(this.width),
 			y: random(this.height)
 		});
@@ -20,7 +19,50 @@ function maze(width, height) {
 
 	this.reset();
 
-	this.continueGeneration = function() {
+	this.pickcell = function(current) {
+		toAdd = [
+			{ x: current.x - 1, y: current.y },
+			{ x: current.x + 1, y: current.y },
+			{ x: current.x, y: current.y - 1 },
+			{ x: current.x, y: current.y + 1 }
+		];
+		choices = Array();
+		toAdd.forEach(function(vector) {
+			if (vector.x >= 0 && vector.x < this.width && vector.y >= 0 && vector.y < this.height)
+				if (!this.cellArray[vector.x][vector.y].visited) choices.push(vector);
+		});
+		if (choices.length === 0) return null;
+		else return choices[random(choices.length)];
+	};
 
+	this.continueGeneration = function() {
+		if (this.path.length !== 0) {
+			var current = this.path[this.path.length - 1];
+			var next = this.pickcell(current);
+			if (next === null) {
+				this.path.pop();
+				return this.continueGeneration();
+			} else {
+				this.cellArray[next.x][next.y].visited = true;
+				this.path.push(next);
+				if (next.x === current.x - 1) {
+					this.cellArray[current.x][current.y].setLeft();
+					this.cellArray[next.x][next.y].setRight();
+				} else if (next.x === current.x + 1) {
+					this.cellArray[current.x][current.y].setRight();
+					this.cellArray[next.x][next.y].setLeft();
+				} else if (next.y === current.y - 1) {
+					this.cellArray[current.x][current.y].setUp();
+					this.cellArray[next.x][next.y].setDown();
+				} else if (next.y === current.y + 1) {
+					this.cellArray[current.x][current.y].setDown();
+					this.cellArray[next.x][next.y].setUp();
+				}
+				console.log("coucou");
+			}
+			return false;
+		} else {
+			return true;
+		}
 	};
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,7 @@
 function setup() {
 	maze(10, 10);
+	while (!maze.continuegeneration()) ;
+	console.log(maze);
 }
 
 function draw() {
